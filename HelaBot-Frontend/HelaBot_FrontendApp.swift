9ea06cba01d5280ea57fa8502e7c5b0fbfd4ab3e
guidogimeno <gimenoguido@gmail.com>
2020-10-22 18:27:20 -0300

    Initial Commit

---
+++ HelaBot-Frontend/HelaBot_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelaBot_FrontendApp.swift
+//  HelaBot-Frontend
+//
+//  Created by Guido Gimeno on 22/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct HelaBot_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
